420c1cd9cb0e13f057fb6b83fba108c3b9eb9d55
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-16 07:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function firstWord(s) {
   // your code here
+	if(s.trim() === ''){
+		return '';
+	}
+	let words = s.trim().split(' ');
+	return words[0];
 }
+
 
 // Do not change the code below
 
